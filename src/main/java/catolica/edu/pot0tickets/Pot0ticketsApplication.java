980710d4ed8145ebb7fb04da45ab9625abd47cbd
xaldakr <50474978+xaldakr@users.noreply.github.com>
2024-10-26 22:24:19 -0600

    commit potCero

---
+++ src/main/java/catolica/edu/pot0tickets/Pot0ticketsApplication.java
@@ -0,0 +1,13 @@
+package catolica.edu.pot0tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pot0ticketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pot0ticketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/catolica/edu/pot0tickets/Pot0ticketsApplicationTests.java
@@ -0,0 +1,13 @@
+package catolica.edu.pot0tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pot0ticketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
